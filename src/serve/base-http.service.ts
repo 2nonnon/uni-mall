a5304cdcccc02fa43b9d9d97f598a5468113eba6
2nonnon <lanpangzi0205@gmail.com>
2022-05-06 22:30:32 +0800

    fix: 修复请求头配置、非200响应处理

--- src/serve/base-http.service.ts
+++ src/serve/base-http.service.ts
@@ -9,16 +9,17 @@ export default class BaseHttpService {
     Object.assign(options, this._getCommonOptions())
 
     return new Promise<T>((resolve, reject) => {
+      console.log(options)
       uni.request({
         method: 'GET',
         url: `${this.baseUrl}/${endpoint}`,
         header: options,
         success: (res) => {
-          const data = res.data as T
-          resolve(data)
-        },
-        fail: (err) => {
-          reject(err)
+          if (res.statusCode === 200 || res.statusCode === 201) resolve(res.data as T)
+          else {
+            this._handleHttpError(res)
+            reject(res.data)
+          }
         }
       });
     })
@@ -38,11 +39,11 @@ export default class BaseHttpService {
         header: options,
         data,
         success: (res) => {
-          const data = res.data as T
-          resolve(data)
-        },
-        fail: (err) => {
-          reject(err)
+          if (res.statusCode === 200 || res.statusCode === 201) resolve(res.data as T)
+          else {
+            this._handleHttpError(res)
+            reject(res.data)
+          }
         }
       });
     })
@@ -60,41 +61,37 @@ export default class BaseHttpService {
         url: `${this.baseUrl}/${endpoint}`,
         header: options,
         success: (res) => {
-          const data = res.data as T
-          resolve(data)
-        },
-        fail: (err) => {
-          reject(err)
+          if (res.statusCode === 200 || res.statusCode === 201) resolve(res.data as T)
+          else {
+            this._handleHttpError(res)
+            reject(res.data)
+          }
         }
       });
     })
   }
 
   protected _handleHttpError(error: any) {
-    console.log(error.response.data)
-    const { statusCode } = error.response.data
+    const { statusCode } = error
     if (statusCode === 401) {
       this._handle401()
     }
-    return Promise.reject(error)
   }
 
   protected _handle401(): void {
-    console.log(401)
+    this.removeToken()
   }
 
   protected _getCommonOptions() {
-    const token = this._accessToken
+    const token = this.accessToken
 
     return {
-      headers: {
         Authorization: `Bearer ${token}`,
         'Content-Type': 'application/json;charset=utf-8'
       }
-    }
   }
 
-  protected get accessToken(): string | null {
+  get accessToken(): string | null {
     return this._accessToken ? this._accessToken : this.loadToken()
   }
 
